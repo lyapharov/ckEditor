d050cd4c448e2bf95917d31473e3f4c64ae7fe41
Timur Useinudzha <timur4iku@gmail.com>
2015-10-23 20:41:38 +0300

    Allow data-id attributes

--- config.js
+++ config.js
@@ -35,6 +35,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 	config.title = false;
+	config.extraAllowedContent = 'p[!data-id];';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
